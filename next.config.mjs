b7ef61bd669ddcefd5051d777a2263c759717ecd
Eren Vys <veyisoglueren@gmail.com>
2025-01-20 16:11:58 +0300

    small modification

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["mbtizstlhxeezasdfcop.supabase.co"],
+    domains: [
+      "mbtizstlhxeezasdfcop.supabase.co",
+      "avatars.githubusercontent.com",
+    ],
   },
 };
 
